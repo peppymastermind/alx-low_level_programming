e313bfdc3c02c0a270f18504863d019638b0dcad
peppymastermind <koranchieperpetual94@gmail.com>
2023-03-20 15:17:50 -0700

    Make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
-#!/bin/basih
+#!/bin/bash
 wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
+
